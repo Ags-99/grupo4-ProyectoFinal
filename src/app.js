41aaa04387b32bde9b5c2d6e55c6455d58e9d3e0
Ags-99 <paredesagustin477@gmail.com>
2024-05-23 12:00:32 -0300

    haciendo responsive el nav, slogan, offer y aboutUs

---
+++ src/app.js
@@ -0,0 +1,28 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const togglerBars = document.getElementById("togglerBars");
+    const togglerXmark = document.getElementById("togglerXmark");
+    const categoryContainer = document.querySelector(".category_container");
+
+    togglerBars.addEventListener('click', function () {
+        if (window.innerWidth < 768) {
+            if (categoryContainer.style.display === "none" || categoryContainer.style.display === "") {
+                categoryContainer.style.display = "block";
+            } else {
+                categoryContainer.style.display = "none";
+            }
+        }
+    })
+
+    togglerXmark.addEventListener('click', function () {
+        if (window.innerWidth < 768) {
+            if (categoryContainer.style.display === "block" || categoryContainer.style.display === "") {
+                categoryContainer.style.display = "none";
+            } else {
+                categoryContainer.style.display = "none";
+            }
+        }
+    })
+
+
+});
